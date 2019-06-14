48f2f42917f8c901e2bf2e1300c244ad8503ec8a
CNflysky <cnflysky@qq.com>
2019-06-14 16:35:46 +0800

    0

--- install.sh
+++ install.sh
@@ -411,8 +411,8 @@ else
 read -p "发现新版本$scriptver,是否升级脚本?请输入yes确认升级!" update
 if [ "$update" = "yes" ]; then
 wget -N --no-check-certificate https://raw.githubusercontent.com/CNflysky/BDSDeploy/master/install.sh
-chmod +x install_latest.sh
 echo "升级完成!"
+exit
 else
 echo "中止."
 fi
